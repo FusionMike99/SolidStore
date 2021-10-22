899e68a719f3c82bba89b7fdb015ea1f250dbf1a
Mykhailo Teziaiev <fusionmike99@gmail.com>
2021-10-22 20:54:14 +0300

    Edited ProductRepositoryTest

--- StoreSolidTestProject/ProductRepositoryTest.cs
+++ StoreSolidTestProject/ProductRepositoryTest.cs
@@ -237,17 +237,5 @@ namespace TestProject1
             //Assert
             Assert.AreEqual(expectedEx, actualEx.GetType());
         }
-
-        private List<Product> GetProducts()
-        {
-            return new List<Product>()
-            {
-                new Product("56cdec93-c3e9-4888-b2aa-7f252ced961f", "product1", "category1", "description1", 45.5F),
-                new Product("6bc94b71-8de7-4120-9785-b223af30a4b7", "product2", "category1", "description2", 3F),
-                new Product("09342426-64f8-46c4-b1c3-2e204638d0c5", "product3", "category2", "description3", 41F),
-                new Product("72bbe220-6c7a-4b22-be76-aa6b662466db", "product4", "category2", "description4", 89.6F),
-                new Product("c579271e-32a4-479b-a8e7-c34df5d56e3b", "product5", "category3", "description5", 53F)
-            };
-        }
     }
 }
